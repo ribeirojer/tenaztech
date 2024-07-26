bba4306bce0a7233374291257c91cc64109daefd
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2024-07-26 00:36:13 -0300

    feat: edit header and layout transition

--- server/infrastructure/config/logger.ts
+++ server/infrastructure/config/logger.ts
@@ -3,16 +3,17 @@ import * as log from "https://deno.land/std@0.224.0/log/mod.ts";
 log.setup({
 	handlers: {
 		console: new log.ConsoleHandler("DEBUG"),
-		file: new log.FileHandler("WARN", {
+		/*file: new log.FileHandler("WARN", {
 			filename: "./log.txt",
 			formatter: (record) =>
 				`${record.datetime.toISOString()} [${record.levelName}] ${record.msg}`,
-		}),
+		}),*/
 	},
 	loggers: {
 		default: {
 			level: "DEBUG",
-			handlers: ["console", "file"],
+			handlers: ["console"],
+			//handlers: ["console", "file"],
 		},
 	},
 });
