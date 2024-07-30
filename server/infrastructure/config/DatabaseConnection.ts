ece4e8a96621a22c80821805441410568ecf3a08
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2024-07-29 23:34:37 -0300

    fix: bug at deploy .env

--- server/infrastructure/config/DatabaseConnection.ts
+++ server/infrastructure/config/DatabaseConnection.ts
@@ -1,12 +1,14 @@
 import { type SupabaseClient, createClient } from "jsr:@supabase/supabase-js@2";
-import "jsr:@std/dotenv/load";
+import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
+const env = await load();
 
-const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
+const supabaseUrl = env.SUPABASE_URL || Deno.env.get("SUPABASE_URL");
+const supabaseKey = env.SUPABASE_KEY || Deno.env.get("SUPABASE_KEY");
+
 if (!supabaseUrl) {
 	throw new Error("SUPABASE_URL is not defined in .env");
 }
 
-const supabaseKey = Deno.env.get("SUPABASE_KEY")!;
 if (!supabaseKey) {
 	throw new Error("SUPABASE_KEY is not defined in .env");
 }
